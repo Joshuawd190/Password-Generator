a628448a2fde301da58958915428a90caec70ad9
Joshua D Wood <joshuawd190@gmail.com>
2021-04-11 15:53:50 -0500

    added length prompt and validation

--- Develop/script.js
+++ Develop/script.js
@@ -2,7 +2,7 @@
 
 //jsObj to hold password criteria
 var passwordData = {
-  length: 8,
+  length: 0,
   hasLowercase: true,
   hasUppercase: true,
   hasNumber: true,
@@ -17,8 +17,40 @@ var passwordData = {
   },
 };
 
+//regex to test pass string for criteria
+const testNum = new RegExp(/\d+/);
+const testLower = new RegExp(/[a-z]+/);
+const testUpper = new RegExp(/[A-Z]+/);
+const testSpec = new RegExp(/[^0-9][^a-z][^A-Z]/);
+
 // Generate password function to hook into exsisting code
-var generatePassword = function () {};
+var generatePassword = function () {
+  //window prompt password length
+  passwordData.length = parseInt(
+    window.prompt("Please choose a length between 8 and 128 for your password")
+  );
+  //re prompt if inputValid returns false
+  if (!passwordData.inputValid()) {
+    window.alert("Please input a valid password length!");
+    generatePassword();
+  }
+
+  //window confirm password criteria
+  //validate that at least one char is true
+  //generate random pass
+  //26 letters, 33 special char, 10 num(incuding 0)
+  //randomly choose category
+  //if lower, iterate letter list
+  //if uppercase, flip a coin to determine case
+  //if special char, iterate char list
+  //if number, iterate num list
+  //return char
+  //add to array
+  //set new var to join array
+  //validate matches criteria
+  //use a regex? and .test()
+  //return pass to be printed on page
+};
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
