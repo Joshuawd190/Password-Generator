8bb634ae98a6fbca5afd3772e1eecefac3cb7bf5
Joshua D Wood <joshuawd190@gmail.com>
2021-04-11 14:40:39 -0500

    "Added js obj to hold password criteria"

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,24 @@
 // Assignment code here
 
+//jsObj to hold password criteria
+var passwordData = {
+  length: 8,
+  hasLowercase: true,
+  hasUppercase: true,
+  hasNumber: true,
+  hasSpecial: true,
 
+  inputValid: function () {
+    if (this.length >= 8 && this.length <= 128) {
+      return true;
+    } else {
+      return false;
+    }
+  },
+};
+
+// Generate password function to hook into exsisting code
+var generatePassword = function () {};
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
@@ -10,7 +28,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
